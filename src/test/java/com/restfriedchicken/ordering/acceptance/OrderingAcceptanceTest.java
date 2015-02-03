e1c2a8c2596b135e4052b0c5865f287765a6e704
hippoom <hippoom@gmail.com>
2015-02-03 16:49:48 +0800

    eliminate hard code config in acceptance tests.

--- src/test/java/com/restfriedchicken/ordering/acceptance/OrderingAcceptanceTest.java
+++ src/test/java/com/restfriedchicken/ordering/acceptance/OrderingAcceptanceTest.java
@@ -6,13 +6,17 @@ import com.restfriedchicken.ordering.rest.OrderResource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.hateoas.RelProvider;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.hal.Jackson2HalModule;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
@@ -22,13 +26,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+import static java.lang.String.format;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class, classes = {
+        OrderingAcceptanceTest.EnvConfig.class,
+        OrderingAcceptanceTest.HalConfiguration.class}
+)
 public class OrderingAcceptanceTest {
 
+    @Autowired
+    private EnvConfig env;
 
     @Autowired
     private RestTemplate restTemplate;
@@ -40,14 +50,34 @@ public class OrderingAcceptanceTest {
                 trackingId, new PlaceOrderCommand.Item("fried chicken", 1));
 
         final OrderResource resource = restTemplate.
-                postForObject("http://192.168.70.2:18001/order", command, OrderResource.class);
+                postForObject(env.orderingServiceUrl() + "/order", command, OrderResource.class);
 
         assertThat(resource.getLink("payment").getHref(), is("http://www.restfriedchicken.com/online-txn/" + trackingId));
     }
 
+
+    @Component
+    @PropertySource("classpath:env.properties")
+    static class EnvConfig {
+        @Value("${orderingServiceHost}")
+        private String orderingServiceHost;
+        @Value("${orderingServicePort}")
+        private int orderingServicePort;
+
+        public String orderingServiceUrl() {
+            return format("http://%s:%d", orderingServiceHost, orderingServicePort);
+        }
+    }
+
     @Configuration
     @EnableHypermediaSupport(type = {EnableHypermediaSupport.HypermediaType.HAL})
-    static class ContextConfiguration {
+    static class HalConfiguration {
+
+        //You need this
+        @Bean
+        public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+            return new PropertySourcesPlaceholderConfigurer();
+        }
 
         @Bean
         RestTemplate restTemplate() {
@@ -75,6 +105,8 @@ public class OrderingAcceptanceTest {
             halConverter.setObjectMapper(halObjectMapper);
             return halConverter;
         }
+
+
     }
 
 
