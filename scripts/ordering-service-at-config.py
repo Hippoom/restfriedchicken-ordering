57dba30b3f5f305f0c89cb4f546ce393ca9b6844
hippoom <hippoom@gmail.com>
2015-02-04 09:34:09 +0800

    at config.

---
+++ scripts/ordering-service-at-config.py
@@ -0,0 +1,10 @@
+import os
+import sys
+
+args = sys.argv
+env = args[1]
+
+command = 'cd ansible && ansible-playbook ordering-service-at-config.yml -i ../../environments/{env}/inventory -vvvv --extra-vars="env={env}"'
+
+os.system(command.format(env=env))
+
