8470318d6169665c717460c8b54a0da5c49ead7f
hippoom <hippoom@gmail.com>
2015-05-07 16:52:20 +0800

    Adopting soft links to implement deploy and rollback. Now one can rollback to previsous release by typing python scripts/ordering-service-deploy.py  rollback. #3

--- scripts/ordering-service-deploy.py
+++ scripts/ordering-service-deploy.py
@@ -4,6 +4,15 @@ import os
 
 args = sys.argv
 env = args[1]
+if len(args) == 3:
+    action = args[2]
+else:
+    action = 'deploy'
+
+if action == 'deploy':
+    skip_tags = 'rollback'
+else:
+    skip_tags = 'deploy'
 
 dir=os.path.dirname(os.path.realpath(__file__))
 
@@ -11,7 +20,7 @@ binary_name=open('{dir}/../build/libs/binary'.format(dir=dir)).read()
 
 workspace=os.path.abspath(os.path.join(os.path.join(os.path.realpath(__file__), os.pardir), os.pardir))
 
-command = 'ansible-playbook -i {dir}/../environments/{env}/inventory {dir}/ansible/ordering-service-vm-deployment.yml --extra-vars="workspace={workspace} env={env} ordering_binary={binary_name}"'
+command = 'ansible-playbook -i {dir}/../environments/{env}/inventory {dir}/ansible/ordering-service-vm-deployment.yml --skip-tags "{skip_tags}" --extra-vars="workspace={workspace} env={env} ordering_binary={binary_name}"'
 
-subprocess.check_call(command.format(workspace=workspace, env=env, dir=dir, binary_name=binary_name), shell=True)
+subprocess.check_call(command.format(skip_tags=skip_tags, workspace=workspace, env=env, dir=dir, binary_name=binary_name), shell=True)
 
