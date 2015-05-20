52d39a39a6ffcfa54b518f8f0b5e50a4dfc99a97
hippoom <hippoom@gmail.com>
2015-05-20 15:26:20 +0800

    Fix issue 8, introducing spring-test-dbunit-test-template to eliminate data duplicate in persistenct test. #8

--- src/test/java/com/restfriedchicken/ordering/persistence/hibernate/HibernateOrderRepositoryTest.java
+++ src/test/java/com/restfriedchicken/ordering/persistence/hibernate/HibernateOrderRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.restfriedchicken.ordering.persistence.hibernate;
 
+import com.github.hippoom.xunittemplate.springtestdbunit.dataset.CreateTemplateModifier;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.ExpectedDatabase;
@@ -36,26 +37,37 @@ import static org.modelmapper.config.Configuration.AccessLevel.*;
 @FlywayTest(invokeCleanDB = false)
 public class HibernateOrderRepositoryTest {
 
+    public static final String TRACKING_ID_OF_PROTOTYPE = "240eff2f-6c38-4998-9287-2e447dac4fd3";
+    public static final String TRACKING_ID_OF_TO_BE_SAVED = "240eff2f-6c38-4998-9287-2e447dac4fd4";
+
     @Autowired
     private PlatformTransactionManager transactionManager;
 
     @Autowired
     private OrderRepository subject;
 
-    @DatabaseSetup("classpath:order_save_fixture.xml")
-    @ExpectedDatabase(value = "classpath:order_save_expected.xml", assertionMode = NON_STRICT_UNORDERED)
+
+    @DatabaseSetup("classpath:order_save_template.xml")
+    @ExpectedDatabase(value = "classpath:order_save_template.xml",
+            assertionMode = NON_STRICT_UNORDERED,
+            modifiers = {OrderCreateTemplateModifier.class})
     @Test
     public void should_saves_order() throws Exception {
-        final String trackingIdOfPrototype = "240eff2f-6c38-4998-9287-2e447dac4fd3";
-        final String trackingIdOfToBeSaved = "240eff2f-6c38-4998-9287-2e447dac4fd4";
 
         final Order toBeSaved = new TransactionTemplate(transactionManager)
                 .execute(status -> {
-                    final Optional<Order> orderMaybe = subject.findByTrackingId(trackingIdOfPrototype);
-                    return clone(orderMaybe.get(), trackingIdOfToBeSaved);
+                    final Optional<Order> orderMaybe = subject.findByTrackingId(TRACKING_ID_OF_PROTOTYPE);
+                    return clone(orderMaybe.get(), TRACKING_ID_OF_TO_BE_SAVED);
                 });
 
         subject.store(toBeSaved);
+    }
+
+    private class OrderCreateTemplateModifier extends CreateTemplateModifier {
+
+        public OrderCreateTemplateModifier() {
+            super("tracking_id", TRACKING_ID_OF_TO_BE_SAVED);
+        }
     }
 
     private Order clone(Order prototype, String newTrackingId) {
